e6822ec50e1320883b8bec6bfe05f1fd16a09bf6
bobfishtincar <bhavishdinakar@berkeley.edu>
2017-01-06 16:29:32 -0800

    can now view list of items

--- findstuff/www/js/app.js
+++ findstuff/www/js/app.js
@@ -1,6 +1,6 @@
 angular.module('starter', ['ionic'])
 
-.controller('AppCtrl', function($scope, $ionicPopup) {
+.controller('AppCtrl', function($scope, $ionicPopup, $ionicModal) {
   // $scope.data = {
   //   speechText: ''
   // };
@@ -32,6 +32,36 @@ angular.module('starter', ['ionic'])
   $scope.itemname = '';
   $scope.itemlocation = '';
   $scope.searcheditem = '';
+  var trigger = false;
+
+  $ionicModal.fromTemplateUrl('modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    firebase.database().ref(firebase.auth().currentUser.uid).on('value', function(snapshot) {
+      var group = {};
+      var i = 0;
+      snapshot.forEach(function(childSnapshot) {
+        var temp = {};
+        temp.name = childSnapshot.key;
+        temp.location = childSnapshot.val();
+        group[i] = temp;
+        i++;
+      });
+      $scope.group = group;
+      $scope.modal.show();
+    });
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
 
   $scope.storeItemName = function() {
     var recognition = new SpeechRecognition();
@@ -69,22 +99,46 @@ angular.module('starter', ['ionic'])
   };
 
   $scope.searchItem = function() {
+    trigger = true;
     var recognition = new SpeechRecognition();
       recognition.onresult = function(event) {
           if (event.results.length > 0) {
               $scope.searcheditem = event.results[0][0].transcript;
               $scope.$apply();
               firebase.database().ref(firebase.auth().currentUser.uid + '/' + $scope.searcheditem).on('value', function(snapshot) {
-                  if (snapshot.val()) {
-                    $ionicPopup.alert({
-                      title: $scope.searcheditem,
-                      template: snapshot.val()
-                    });
-                  } else {
-                    $ionicPopup.alert({
-                      title: $scope.searcheditem,
-                      template: 'Could not find item.'
-                    });
+                  if (trigger) {
+                    if (snapshot.val()) {
+                      TTS.speak({
+                               text: snapshot.val(),
+                               locale: 'en-GB',
+                               rate: 1.5
+                           }, function () {
+                               // Do Something after success
+                           }, function (reason) {
+                               // Handle the error case
+                           });
+                      $ionicPopup.alert({
+                        title: $scope.searcheditem,
+                        template: snapshot.val()
+                      });
+                    } else {
+                      TTS.speak({
+                               text: 'Could not find item.',
+                               locale: 'en-GB',
+                               rate: 1.5
+                           }, function () {
+                               // Do Something after success
+                           }, function (reason) {
+                               // Handle the error case
+                           });
+                      $ionicPopup.alert({
+                        title: $scope.searcheditem,
+                        template: 'Could not find item.'
+                      });
+                    }
+                    $scope.searcheditem = '';
+                    $scope.$apply();
+                    trigger = false;
                   }
               });
           }
